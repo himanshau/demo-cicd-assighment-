7d5abe8163a28b2bebaecd6e9bac2627c8cfbb5c
himanshau <hs9011016@gmail.com>
2025-03-06 20:58:52 +0530

    rth commit

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+import base64
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, Flask with CI/CD!"
+# Base64-encoded image string
+image_base64 = "/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBwgHBgkIBwgKCgkLDRYPDQwMDRsUFRAWIB0iIiAdHx8kKDQsJCYxJx8fLT0tMTU3Ojo6Iys/RD84QzQ5OjcBCgoKDQwNGg8PGjclHyU3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3N//AABEIAJQAlAMBIg..."
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route("/")
+def home():
+    return render_template("index.html", image_data=image_base64)
+
+if __name__ == "__main__":
+    app.run(debug=True)
